00f7f094fdc5d226bc0d2f259cdf7c6f51e5de16
mooon <huangb@cadg.cn>
2021-10-08 14:33:11 +0800

    add generator log test api

---
+++ demo-logback/src/main/java/com/example/DemoController.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import com.cbim.logging.AuditLog;
+import com.cbim.logging.CbimLoggerFactory;
+import com.cbim.logging.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class DemoController {
+
+    static final Logger logger = LoggerFactory.getLogger(ExampleApp.class);
+    static final CbimLoggerFactory AUDIT_LOGGER = CbimLoggerFactory.getAuditLogger("auditLogger");
+    static final Logger xxxBusLogger = LoggerFactory.getLogger("xxxBusinessLogger");
+
+    @GetMapping("/test1")
+    public Object auditTest() {
+        // runtime log
+        logger.info("this is runtime logback");
+        // audit log
+        Random random = new Random();
+        int rInt = random.nextInt();
+        AUDIT_LOGGER.audit(AuditLog.builder("dms",
+                        "accountId-" + rInt,
+                        "operatorId-" + rInt,
+                        "UserLogin")
+                .operator("LiSi")
+                .note("用户登录Logback")
+                .result(Result.FAILED)
+                .exception(new IllegalArgumentException("\"param error\"").toString())
+                .build());
+        // business log
+        xxxBusLogger.info("this is business logback.");
+        return "OK.";
+    }
+
+}
--- demo-logback/src/main/java/com/example/ExampleApp.java
+++ demo-logback/src/main/java/com/example/ExampleApp.java
@@ -14,7 +14,7 @@ public class ExampleApp {
     static final Logger logger = LoggerFactory.getLogger(ExampleApp.class);
     // audit test
     //static final Logger auditLogger = LoggerFactory.getLogger("auditLogger");
-    static final CbimLoggerFactory auditLogger = CbimLoggerFactory.getAuditLogger("auditLogger");
+    static final CbimLoggerFactory AUDIT_LOGGER = CbimLoggerFactory.getAuditLogger("auditLogger");
     // xxx business test
     static final Logger xxxBusLogger = LoggerFactory.getLogger("xxxBusinessLogger");
     public static void main(String[] args) {
@@ -22,7 +22,7 @@ public class ExampleApp {
         // runtime log
         logger.info("this is runtime logback");
         // audit log
-        auditLogger.audit(AuditLog.builder("dms", "accountId", "operatorId", "UserLogin")
+        AUDIT_LOGGER.audit(AuditLog.builder("dms", "accountId", "operatorId", "UserLogin")
                 .operator("lisi")
                 .note("用户登录LogBack")
                 .result(Result.FAILED)
